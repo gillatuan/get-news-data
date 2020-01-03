4d0527fba30c9d8bfb03c34bb42ccd3f666dccd2
Bui Doan Ngoc Tuan <tuan.buidoanngoc@gmail.com>
2020-01-03 16:51:09 +0700

    udpate package and reformat code

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://test:test123@ds055680.mlab.com:55680/tuandevconnector',
+  mongoURI: 'mongodb+srv://fordemo:l1mIjE7h4rOLni7z@cluster0-6k9d5.mongodb.net/test?retryWrites=true&w=majority',
   secretOrKey: 'secret'
 };
