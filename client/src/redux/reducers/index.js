eee15fa779439c2ce96f0e96d58b914c84d5fa5e
Bui Doan Ngoc Tuan <tuan.buidoanngoc@gmail.com>
2020-01-04 14:30:03 +0700

    remove some filés not use

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,18 +1,12 @@
 import { combineReducers } from "redux"
 import authReducer from "./authReducer"
 import commonReducer from "./commonReducer"
-import mediaReducer from "./mediaReducer"
 import profileReducer from "./profileReducer"
-import postReducer from "./postsReducer"
-import modulesReducer from "./modulesReducer"
 
 export default combineReducers({
   auth: authReducer,
   common: commonReducer,
-  media: mediaReducer,
-  modules: modulesReducer,
-  profile: profileReducer,
-  posts: postReducer,
+  profile: profileReducer
 })
 
 /* export const mapStateToProps = (state, props) => {
